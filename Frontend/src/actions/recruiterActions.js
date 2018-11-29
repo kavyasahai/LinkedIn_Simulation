178ebba99268b6e15f03175ce2361ccace0054c9
Sheena Gupta <sheena.gupta.in@gmail.com>
2018-11-29 12:46:04 -0800

    Update recruiterActions.js

--- Frontend/src/actions/recruiterActions.js
+++ Frontend/src/actions/recruiterActions.js
@@ -1,22 +1,24 @@
-import axios from "axios"
-import { POSTED_APPLICATIONS } from './types'
-import { getToken } from '../common/auth'
+import axios from "axios";
+import { POSTED_APPLICATIONS } from "./types";
+import { getToken } from "../components/common/auth";
 
-const ROOT_URL = "http://localhost:3001"
+const ROOT_URL = "http://localhost:3001";
 
 export const getPostedApplications = () => dispatch => {
-  axios.get({
-    method: 'get',
-    url: ROOT_URL + '/recruiter/posted_applications',
-    headers: {
-      Authorization: getToken()
-    }
-  }).then(res => {
-    if (res.data.status === "OK") {
-      dispatch({
-        type: POSTED_APPLICATIONS,
-        payload: res.data.payload
-      })
-    }
-  })
-}
+  axios
+    .get({
+      method: "get",
+      url: ROOT_URL + "/recruiter/posted_applications",
+      headers: {
+        Authorization: getToken()
+      }
+    })
+    .then(res => {
+      if (res.data.status === "OK") {
+        dispatch({
+          type: POSTED_APPLICATIONS,
+          payload: res.data.payload
+        });
+      }
+    });
+};
