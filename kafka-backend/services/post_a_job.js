b302d3fd63f8881e3f2f0bc595b4fb3dc8d1538e
Tong Wu <tongwu@live.com>
2018-11-28 23:46:47 -0800

    Transform a user into recruiter if a job is posted

--- kafka-backend/services/post_a_job.js
+++ kafka-backend/services/post_a_job.js
@@ -1,8 +1,14 @@
 var { Jobs } = require('../../Backend/db/schema')
+var UserSchema = require('../../Backend/models/user')
+
 function handle_request(msg, callback) {
   var newJobs = new Jobs(msg)
   newJobs.save().then(() => {
     console.log('job posted')
+    var email = msg.adminId
+    UserSchema.findOneAndUpdate({ email }, { isRecruiter: true })
+      .then(() => console.log('user is recruiter'))
+      .catch(() => console.log('update user as recruiter failed'))
     callback(null, msg)
   }).catch(err => {
     callback(err.errmsg, [])
