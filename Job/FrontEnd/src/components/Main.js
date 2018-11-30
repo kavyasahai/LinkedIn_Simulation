79e223f942980151e3d79fd0f05812440fefad12
Aishwariya <bhattaishwariya@gmail.com>
2018-11-29 18:17:10 -0600

    Changes

--- Job/FrontEnd/src/components/Main.js
+++ Job/FrontEnd/src/components/Main.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
 import Home from './JobSearch';
 import Detail from './Detail';
+import Apply from "./Application"
 
 //Create a Main Component
 class Main extends Component {
@@ -11,6 +12,7 @@ class Main extends Component {
                 {/*Render Different Component based on Route*/}
                 <Route exact path="/" component={Home}/>
                 <Route path="/Detail" component={Detail}/>
+                <Route path="/Apply" component={Apply}/>
        
               
             </div>
