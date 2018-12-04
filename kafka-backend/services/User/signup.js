aecddf00c92dab55df725bb7a031ab307f035614
Sheena Gupta <sheena.gupta.in@gmail.com>
2018-12-04 03:54:36 -0800

    sign-up bug fix

--- kafka-backend/services/User/signup.js
+++ kafka-backend/services/User/signup.js
@@ -4,7 +4,7 @@ var Users = require("../../models/user");
 require("../../db/mongoose");
 
 function handle_request(msg, callback) {
-  var username=msg.username.toLowerCase();
+  var username = msg.username.toLowerCase();
   var newUser = {
     username: username,
     password: msg.password,
@@ -17,37 +17,29 @@ function handle_request(msg, callback) {
     newUser,
     function(res) {
       console.log("user created");
-      // var resData = {
-      //   username: msg.email,
-      //   password: msg.password,
-      //   firstname: msg.firstname,
-      //   lastname: msg.lastname
-      // };
-      // callback(null, resData);
+      //save user details in MongoDB
+      var user = new Users({
+        email: username,
+        firstname: msg.firstname,
+        lastname: msg.lastname
+      });
+
+      console.log("USER+++++++", user);
+      user.save().then(
+        user => {
+          console.log("Sign-up successful.");
+          callback(null, user);
+        },
+        err => {
+          console.log("Could not sign-up");
+          callback(null, []);
+        }
+      );
     },
     function(err) {
       console.log("error creating user");
       console.log(err);
-      //  callback(null, []);
-    }
-  );
-
-  //save user details in MongoDB
-  var user = new Users({
-    email: username,
-    firstname: msg.firstname,
-    lastname: msg.lastname
-  });
-
-  console.log("USER+++++++", user);
-  user.save().then(
-    user => {
-      console.log("Sign-up successful.");
-      callback(null, user);
-    },
-    err => {
-      console.log("Could not sign-up");
-      callback(null, []);
+      callback(null, "error");
     }
   );
 }
